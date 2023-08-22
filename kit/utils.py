19c4f2774b01ea32a6d351adf184ce4462cc27b5
omid <omid9h@gmail.com>
2023-08-22 09:26:35 +0330

    generate_uuid_for_file_with_dir util

--- kit/utils.py
+++ kit/utils.py
@@ -1,3 +1,5 @@
+import os
+import uuid
 from typing import Union
 
 from django.db.models.fields.files import FieldFile, ImageFieldFile
@@ -12,3 +14,13 @@ def remove_field_file(instance_field: Union[FieldFile, ImageFieldFile]) -> None:
 
     if instance_field and os.path.isfile(instance_field.path):
         os.remove(instance_field.path)
+
+
+def generate_uuid_for_file_with_dir(original_file_name: str, upload_dir: str):
+    """
+    generates a uuid name for given file and concats it to given dir
+    Example: <upload_dir>/193f511b-6061-4b03-b136-2d5fa1b3b39b<.extension>
+    """
+    extension = original_file_name.split(".")[-1]
+    file_name = f"{uuid.uuid4()}.{extension}"
+    return os.path.join(upload_dir, file_name)
