03438bc55c76b569886e6c9a9527339d39d750c3
omid <omid9h@gmail.com>
2023-08-22 14:19:20 +0330

    pre-commit and autoflake - reformat all project

---
+++ README.md
@@ -0,0 +1,7 @@
+## notes
+pre-commit
+```bash
+poetry install --dev
+pre-commit install
+pre-commit run -a
+```
--- kit/settings-prod.py
+++ kit/settings-prod.py
@@ -7,3 +7,9 @@ ALLOWED_HOSTS = ["127.0.0.1"]
 CORS_ALLOWED_ORIGINS = [
     # Add allowed origins here
 ]
+
+SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
