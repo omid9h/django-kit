158836a3bd6f7d72451cc98687393830aa8dc96e
omid <omid9h@gmail.com>
2023-08-27 18:24:44 +0330

    in avatar edit creating & checking User object is not necessary

--- userprofile/services.py
+++ userprofile/services.py
@@ -1,7 +1,3 @@
-from django.contrib.auth import get_user_model
-from django.db.models import Q
-from django.utils.translation import gettext_lazy
-
 from kit.services import BaseService
 from kit.utils import remove_field_file
 from userprofile import serializers as s
@@ -13,16 +9,12 @@ class ProfileService(BaseService):
         any authenticated user can edit its own profile"""
         serializer = s.AvatarEditSerializer(data=data)
         serializer.is_valid(raise_exception=True)
-        try:
-            m = get_user_model().objects.get(Q(pk=user.id) & Q(is_active=True))
-        except get_user_model().DoesNotExist:
-            raise ValueError(gettext_lazy("Customer does not exist"))
-        current_avatar = m.avatar
-        m.avatar = serializer.validated_data["avatar"]
-        m.full_clean()
-        m.save()
+        current_avatar = user.avatar
+        user.avatar = serializer.validated_data["avatar"]
+        user.full_clean()
+        user.save()
         remove_field_file(current_avatar)
-        return m
+        return user
 
     def me(self, user):
         serializer = s.MeSerializer(user)
